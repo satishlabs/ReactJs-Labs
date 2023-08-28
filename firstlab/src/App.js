8511c20916a3895af21a8f0df414d12b4b3551ec
satishlabs <satishmca.rnsit@gmail.com>
2023-08-28 18:19:02 +0530

    MyFirst React Lab with class component

---
+++ firstlab/src/App.js
@@ -0,0 +1,15 @@
+
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  render(){
+  return (
+    <div className="myclass">
+      <p className="myclass1">Welcome to SatishLabs!!</p>
+      <p className="myclass2">You are learning ReactJS from SatishLabs</p>
+    </div>
+  );
+}
+}
+export default App;
